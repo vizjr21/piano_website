87ac4bf6b643e0722ecfa1de5e2816434068d69c
Jude Richardson <juderichardson@Judes-MacBook-Air.local>
2025-09-30 19:35:07 +0100

    moved all JS into one file

--- interact.js
+++ interact.js
@@ -14,3 +14,24 @@ function checkWidth() {
     }
 
 }
+
+ // Replace with your CSV export link
+    const sheetUrl = "https://t5sx8gvygh.execute-api.eu-west-2.amazonaws.com/Prod/sheet";
+
+    // Get events data 
+   axios.get(sheetUrl)
+  .then(response => {
+    const values = response.data.values; // Array of rows
+    let html = "<ul>";
+    values.slice(1).forEach(row => { // skip headers
+      const [name, date, location] = row;
+      html += `<li>${name} – ${date} - ${location}</li>`;
+    });
+    html += "</ul>";
+    document.getElementById("data").innerHTML = html;
+  })
+  .catch(err => {
+    document.getElementById("data").innerText = err.message || err;
+  });
+
+  
